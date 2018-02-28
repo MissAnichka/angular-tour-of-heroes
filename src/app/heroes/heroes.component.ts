294e32c58bcb1ab5dec6520465bfcdc956cbfecd
Anna Medyukh <annamedyukh@gmail.com>
2018-02-28 16:58:18 -0500

    added class binding, event binding, ngIf, selection now visible :sparkles:

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,12 +8,12 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Wonder Woman'
-  };
-  
   heroes = HEROES;
+  
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
   
