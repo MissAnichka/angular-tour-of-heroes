44166109fe7c508274c2be5e1e4b02c975cb4214
Anna Medyukh <annamedyukh@gmail.com>
2018-03-01 10:36:37 -0500

    Created messageService, binding to and displaying in messages component :thought_balloon:

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,14 @@ import { Hero } from './heroes/hero';
 import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add("(HeroService): Heroes are flyin at ya!")
     return of(HEROES);
   }
-
-  constructor() { }
-
 }
