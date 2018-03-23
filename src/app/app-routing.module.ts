85941a9dde0aba63e2291e0acc1cd31de2047ef0
Anna Medyukh <annamedyukh@gmail.com>
2018-03-23 16:38:18 -0400

    route guard for hero details added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { CanActivateRouteGuard } from './can-activate-route.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent }
+  { path: 'detail/:id', component: HeroDetailComponent, canActivate: [CanActivateRouteGuard] }
 ];
 
 @NgModule({
